eb5a4f37b2c725050feeaf73bd1421f5bd4bda4c
Le Tien Hai <tienhaile488@gmail.com>
2024-10-05 22:55:14 +0700

    build layout default and header app

---
+++ components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-between items-center mt-10">
+    <NuxtLink to="/" class="text-xl font-bold"> TienHai </NuxtLink>
+    <div>
+      <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+      />
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Tienhai</h1>
+</template>
