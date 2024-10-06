877d220b9532daac26ea76a8fbfab2c469af7bcf
Le Tien Hai <tienhaile488@gmail.com>
2024-10-06 11:08:28 +0700

    trend component icon

---
+++ components/Trend.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="font-bold" :class="[color]">{{ title }}</div>
+
+    <div class="text-2xl font-extrabold text-black dark:text-white mb-2">
+      <USkeleton class="h-8 w-full" v-if="loading" />
+      <div v-else>{{ amount }}</div>
+    </div>
+
+    <div>
+      <USkeleton class="h-6 w-full" v-if="loading" />
+      <div class="flex space-x-1 items-center text-sm" v-else>
+        <UIcon
+          name="i-heroicons-arrow-trending-up"
+          class="w-6 h-6"
+          :class="[color]"
+        />
+        <div class="text-gray-500 dark:text-gray-400">30% vs last period</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  color: String,
+  title: String,
+  amout: Number,
+  lastAmount: Number,
+  loading: Boolean,
+});
+</script>
+
+<style scoped>
+.green {
+  @apply text-green-600 dark:text-green-400;
+}
+.red {
+  @apply text-red-600 dark:text-red-400;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,45 @@
 <template>
-  <section class="flex justify-between items-center mb-10">
-    <h1 class="text-4xl font-extrabold">Summary</h1>
-    <div>
-      <USelectMenu v-model="selectedView" :options="transactionViewOptions" />
-    </div>
-  </section>
+  <div>
+    <section class="flex justify-between items-center mb-10">
+      <h1 class="text-4xl font-extrabold">Summary</h1>
+      <div>
+        <USelectMenu v-model="selectedView" :options="transactionViewOptions" />
+      </div>
+    </section>
+
+    <section
+      class="grid grid-cols-1 sm:grid-cols-2 sm:gap-16 lg:grid-cols-4 mb-10"
+    >
+      <Trend
+        color="green"
+        title="Income"
+        :amount="4000"
+        :last-amount="3000"
+        :loading="false"
+      />
+      <Trend
+        color="red"
+        title="Income"
+        :amount="4000"
+        :last-amount="3000"
+        :loading="false"
+      />
+      <Trend
+        color="red"
+        title="Income"
+        :amount="4000"
+        :last-amount="3000"
+        :loading="false"
+      />
+      <Trend
+        color="green"
+        title="Income"
+        :amount="4000"
+        :last-amount="3000"
+        :loading="true"
+      />
+    </section>
+  </div>
 </template>
 
 <script setup>
