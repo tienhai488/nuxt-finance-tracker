5ec985948960c6ffce4675ad362f4b951362f825
Le Tien Hai <tienhaile488@gmail.com>
2024-10-06 10:36:07 +0700

    select component

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Tienhai</h1>
+  <section class="flex justify-between items-center mb-10">
+    <h1 class="text-4xl font-extrabold">Summary</h1>
+    <div>
+      <USelectMenu v-model="selectedView" :options="transactionViewOptions" />
+    </div>
+  </section>
 </template>
+
+<script setup>
+import { transactionViewOptions } from "~/constants";
+const selectedView = ref(transactionViewOptions[1]);
+</script>
